3f3cd7117162f392052c11b99251048336130096
Matt Jones <bigboxer23@gmail.com>
2023-10-07 14:57:39 -0500

    add site deletion (#12)
    add new site creation QOL improvements

--- src/components/sites/Site.js
+++ src/components/sites/Site.js
@@ -1,32 +1,73 @@
-import { Button } from "react-bootstrap";
+import { Alert, Button, Spinner } from "react-bootstrap";
 import React, { useState } from "react";
 import Device from "./Device";
 import { AiOutlineDelete } from "react-icons/ai";
+import { deleteDevice } from "../../services/services";
 
-const Site = ({ data, devices, setDevices }) => {
-  const [site, setSite] = useState(data);
-
+const Site = ({ data, devices, setDevices, setActiveSite }) => {
+  const [site] = useState(data);
+  const [deleteSiteWarning, setDeleteSiteWarning] = useState(false);
   const deviceUI = devices
     .filter((device) => device.site === site.name)
     .map((device) => {
       return <Device key={device.id} data={device} setDevices={setDevices} />;
     });
 
+  const removeSite = () => {
+    let button = document.getElementById("delete" + site.id);
+    button.classList.add("disabled");
+    deleteDevice(site.id)
+      .then((data) => {
+        setDevices((d) => d.filter((device) => device.id !== site.id));
+        setActiveSite("No Site");
+        button.classList.remove("disabled");
+      })
+      .catch((e) => {
+        console.log(e);
+        button.classList.remove("disabled");
+      });
+  };
   return (
     <div>
       {deviceUI}
       <Button
-        onClick={() => {
-          //setDeleteAcctWarning(false);
-          //updateCustomer({ ...customerData, active: false });
-          //TODO:
-        }}
+        onClick={() => setDeleteSiteWarning(true)}
         variant="danger"
-        className={"ms-2"}
+        className={"ms-2 mb-3"}
+        id={"delete" + site.id}
       >
+        <Spinner
+          as="span"
+          animation="border"
+          size="sm"
+          role="status"
+          className={"me-2 d-none"}
+        />
         <AiOutlineDelete style={{ marginBottom: "2px", marginRight: "6px" }} />
         Delete Site
       </Button>
+      <Alert show={deleteSiteWarning} variant="danger">
+        <p>Are you sure you want to delete this site?</p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button
+            onClick={() => setDeleteSiteWarning(false)}
+            variant="outline-secondary"
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              setDeleteSiteWarning(false);
+              removeSite();
+            }}
+            variant="outline-danger"
+            className={"ms-2"}
+          >
+            Delete Site
+          </Button>
+        </div>
+      </Alert>
     </div>
   );
 };
