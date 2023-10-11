da9350f365bfce57d5ad46e73eba00a655f82fa3
Matt Jones <bigboxer23@gmail.com>
2023-10-10 19:23:44 -0500

    add sticky state util

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 export function preventSubmit(event) {
   if (event.key === "Enter") {
     //Don't submit the form
     event.preventDefault();
     event.stopPropagation();
   }
+}
+
+export function useStickyState(defaultValue, key) {
+  const [value, setValue] = useState(() => {
+    const stickyValue = window.localStorage.getItem(key);
+    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
 }
