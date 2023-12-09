1dd9f0f1981cb0a6180a03664377e5fbed2cb7fa
Matt Jones <bigboxer23@gmail.com>
2023-12-09 16:41:42 -0600

    style: prettier wasn't running

--- src/services/services.js
+++ src/services/services.js
@@ -93,24 +93,24 @@ export function getMaxCurrent(device) {
 }
 
 export function getTileContent(device, offset) {
-	return api.post("search", [
-		getAvgTotalBody(
-				device,
-				offset === HOUR
-						? new Date(new Date().getTime() - offset)
-						: getRoundedTime(false, offset === DAY ? 0 : offset),
-				new Date(),
-		),
-		getMaxCurrentBody(device),
-		getDataPageBody(
-				device.site,
-				device.name,
-				getRoundedTime(false, DAY),
-				getRoundedTime(true, 0),
-				0,
-				1,
-		),
-	]);
+  return api.post("search", [
+    getAvgTotalBody(
+      device,
+      offset === HOUR
+        ? new Date(new Date().getTime() - offset)
+        : getRoundedTime(false, offset === DAY ? 0 : offset),
+      new Date(),
+    ),
+    getMaxCurrentBody(device),
+    getDataPageBody(
+      device.site,
+      device.name,
+      getRoundedTime(false, DAY),
+      getRoundedTime(true, 0),
+      0,
+      1,
+    ),
+  ]);
 }
 
 export function getDataPage(site, device, start, end, offset, size) {
