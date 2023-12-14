e057b60ecbd3b78680b7fa439e97435c1e94a593
Matt Jones <bigboxer23@gmail.com>
2023-12-14 09:36:50 -0600

    fix: correct issue w/wrong label applied on metric tile (#128)

--- src/components/graphs/FormattedLabel.js
+++ src/components/graphs/FormattedLabel.js
@@ -1,11 +1,11 @@
 import { FormattedNumber } from "react-intl";
 
-const FormattedLabel = ({ value, label }) => {
+const FormattedLabel = ({ value, label, unit }) => {
   return value === -1 ? (
     "Loading"
   ) : (
     <div>
-      {label} <FormattedNumber value={value} /> kWH
+      {label} <FormattedNumber value={value} /> {unit}
     </div>
   );
 };
