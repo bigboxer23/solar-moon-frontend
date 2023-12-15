a58c5e41cc6a34a064547efe524b7c3629fe8e21
Alexander Decker <deckeraga@gmail.com>
2023-12-14 18:02:24 -0800

    lint from root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/components/newUIComponents/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/components/newUIComponents/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        "brand-primary": "#5178c2",
-        "brand-primary-light": "#eef2f9",
-        "brand-secondary": "#f6ce46",
-        "text-secondary": "#a9a9a9",
-        danger: "#b00020",
-        "border-color": "#a9a9a9",
+        'brand-primary': '#5178c2',
+        'brand-primary-light': '#eef2f9',
+        'brand-secondary': '#f6ce46',
+        'text-secondary': '#a9a9a9',
+        danger: '#b00020',
+        'border-color': '#a9a9a9',
       },
       boxShadow: {
-        panel: "0 0 16px 0 rgba(17, 17, 26, 0.05);",
+        panel: '0 0 16px 0 rgba(17, 17, 26, 0.05);',
       },
     },
   },
