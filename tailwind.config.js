6e5785519a37896f8cd550266578597624587159
Alexander Decker <deckeraga@gmail.com>
2024-01-16 18:17:23 -0800

    Fix tailwind path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/newUIComponents/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
