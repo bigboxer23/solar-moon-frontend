e543a93f637adc8ed9a6c0553b5638e67386e3d3
Matt Jones <bigboxer23@gmail.com>
2024-01-31 10:17:03 -0600

    feat: add power block

---
+++ src/components/common/PowerBlock.jsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+
+import PowerIcon from './PowerIcon';
+
+export default function PowerBlock({ className, max, currentPower }) {
+  const getGaugeValue = (max, avg) => {
+    max = max === null ? 0 : max;
+    let scale = 100 / max;
+    return avg === 0 ? avg : Math.round(Math.round(scale * avg));
+  };
+
+  const [percent] = useState(getGaugeValue(max, currentPower));
+
+  return (
+    <div className={classNames('PowerBlock flex space-x-2', className)}>
+      <PowerIcon max={max} percent={percent} />
+      <div className='inline-block self-end text-5xl font-bold leading-[3rem]'>
+        {Math.round(currentPower)}
+      </div>
+      <div className='mb-1 inline-block max-w-[3.3rem] self-end text-base font-bold leading-[1.125rem]'>
+        <div className='text-text-secondary'>kW</div>
+        now
+      </div>
+    </div>
+  );
+}
--- src/components/views/site-list/SiteRow.jsx
+++ src/components/views/site-list/SiteRow.jsx
@@ -9,35 +9,11 @@ import {
   TOTAL_AGGREGATION,
   TOTAL_REAL_POWER,
 } from '../../../services/search';
+import PowerBlock from '../../common/PowerBlock';
 import StatBlock from '../../common/StatBlock';
 import WeatherBlock from '../../common/WeatherBlock';
 
 export default function SiteRow({ site }) {
-  const getGaugeValue = (max, avg) => {
-    max = max === null ? 0 : max;
-    let scale = 100 / max;
-    return avg === 0 ? avg : Math.round(Math.round(scale * avg));
-  };
-
-  const [maxPercent, setMaxPercent] = useState(
-    getGaugeValue(
-      site.siteData.weeklyMaxPower.aggregations['max#max'].value,
-      site.siteData.weeklyMaxPower.hits.hits.length > 0
-        ? site.siteData.weeklyMaxPower.hits.hits[0].fields[TOTAL_REAL_POWER][0]
-        : 0,
-    ),
-  );
-
-  const degree =
-    Math.round(((Math.min(100, maxPercent) / 100) * 180 - 45) * 10) / 10;
-
-  const getGaugeColor = (percent) => {
-    if (percent < 15) return 'bg-red-500';
-    if (percent < 25) return 'bg-yellow-300';
-    if (percent > 110) return 'bg-green-500';
-    return 'bg-brand-primary';
-  };
-
   return (
     <NavLink
       className='group flex items-center rounded-lg p-0 transition-all duration-150 hover:bg-neutral-100 sm:p-4 dark:bg-neutral-700 hover:dark:bg-neutral-600'
@@ -83,21 +59,16 @@ export default function SiteRow({ site }) {
             )}
           </div>
           <div className='mb-2 flex items-end justify-center'>
-            <div
-              className={
-                'relative flex aspect-[2] h-8 items-center justify-center overflow-hidden rounded-t-full ' +
-                getGaugeColor(maxPercent)
+            <PowerBlock
+              currentPower={
+                site.siteData.weeklyMaxPower.hits.hits.length > 0
+                  ? site.siteData.weeklyMaxPower.hits.hits[0].fields[
+                      TOTAL_REAL_POWER
+                    ][0]
+                  : 0
               }
-            >
-              <div
-                className='absolute top-0 aspect-square w-full bg-gradient-to-tr from-transparent from-50% to-neutral-300 to-50% transition-transform duration-500'
-                style={{ transform: `rotate(${degree}deg)` }}
-              ></div>
-              <div className='absolute top-1/4 flex aspect-square w-3/4 justify-center rounded-full bg-white transition-all duration-150 group-hover:bg-neutral-100 dark:bg-neutral-700 dark:group-hover:bg-neutral-600' />
-              <div className='absolute bottom-0 w-full truncate text-center text-sm font-bold leading-none text-black dark:text-neutral-100'>
-                {maxPercent}%
-              </div>
-            </div>
+              max={site.siteData.weeklyMaxPower.aggregations['max#max'].value}
+            />
           </div>
           <div className='col-span-2 ml-4 hidden flex-col justify-center text-sm text-black sm:flex dark:text-neutral-100'>
             <span className='whitespace-nowrap'>
