901959946d8efb71fb2107a955f70a53d9253d48
Alexander Decker <deckeraga@gmail.com>
2024-02-02 23:04:50 -0800

    Mobile tweaks

--- src/components/device-block/DeviceBlock.js
+++ src/components/device-block/DeviceBlock.js
@@ -13,7 +13,7 @@ export default function DeviceBlock({
   const [expandedDevice, setExpandedDevice] = useState(false);
 
   const style = classNames(
-    'DeviceBlock rounded-lg p-6 bg-neutral-50 dark:bg-neutral-600 h-fit',
+    'DeviceBlock rounded-lg p-4 sm:p-6 bg-neutral-50 dark:bg-neutral-600 h-fit',
     className,
   );
 
