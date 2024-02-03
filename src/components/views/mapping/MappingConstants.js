9db18b5739e1e416dc47326e6a0380ca8f410a05
Matt Jones <bigboxer23@gmail.com>
2024-02-02 19:13:42 -0600

    feat: use more common names for mapping fields (#195)

--- src/components/views/mapping/MappingConstants.js
+++ src/components/views/mapping/MappingConstants.js
@@ -1,8 +1,8 @@
-export const AVG_CURRENT = 'Average Current';
-export const AVG_VOLT = 'Average Voltage (L-N)';
-export const TOTAL_PF = 'Total (System) Power Factor';
-export const TOTAL_ENG_CONS = 'Total Energy Consumption';
-export const TOTAL_REAL_POWER = 'Total Real Power';
+export const AVG_CURRENT = 'Current';
+export const AVG_VOLT = 'Voltage';
+export const TOTAL_PF = 'System Power Factor';
+export const TOTAL_ENG_CONS = 'Energy Consumption';
+export const TOTAL_REAL_POWER = 'Real Power';
 export const attributes = [
   AVG_CURRENT,
   AVG_VOLT,
@@ -12,12 +12,14 @@ export const attributes = [
 ];
 
 export const attributeMappings = {};
-attributeMappings['Energy Consumption'] = TOTAL_ENG_CONS;
-attributeMappings['Real Power'] = TOTAL_REAL_POWER;
-attributeMappings['Current'] = AVG_CURRENT;
+attributeMappings['Total Real Power'] = TOTAL_REAL_POWER;
+attributeMappings['Average Current'] = AVG_CURRENT;
 attributeMappings['Voltage, Line to Neutral'] = AVG_VOLT;
+attributeMappings['Average Voltage (L-N)'] = AVG_VOLT;
 attributeMappings['Power Factor'] = TOTAL_PF;
+attributeMappings['Total (System) Power Factor'] = TOTAL_PF;
 attributeMappings['kWh del+rec'] = TOTAL_ENG_CONS;
+attributeMappings['Total Energy Consumption'] = TOTAL_ENG_CONS;
 attributeMappings['I a'] = AVG_CURRENT;
 attributeMappings['Vll ab'] = AVG_VOLT;
 attributeMappings['PF sign tot'] = TOTAL_PF;
