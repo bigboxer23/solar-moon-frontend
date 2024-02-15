0d04bc490662cc5d2c1e988007dc7882c7b99fb0
Alexander Decker <deckeraga@gmail.com>
2024-02-14 17:22:18 -0800

    Implement Theme switcher

---
+++ src/components/common/ThemeSelector.js
@@ -0,0 +1,82 @@
+import Tippy from '@tippyjs/react';
+import classNames from 'classnames';
+import { useState } from 'react';
+import { LuMoon, LuSun } from 'react-icons/lu';
+import { MdComputer } from 'react-icons/md';
+
+export default function ThemeSelector() {
+  const [activeTheme, setActiveTheme] = useState(localStorage.theme || '');
+
+  function onThemeChange(theme) {
+    setActiveTheme(theme);
+
+    if (theme === '') {
+      localStorage.removeItem('theme');
+    } else {
+      localStorage.theme = theme;
+    }
+
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  const themeButtonStyle =
+    'flex-1 flex justify-center items-center py-2 transition-colors duration-300 ease-in-out';
+  const themeButtonActiveStyle =
+    'bg-gray-500 text-gray-100 dark:bg-gray-100 dark:text-gray-900';
+  const themeButtonInactiveStyle =
+    'hover:bg-gray-300 dark:hover:bg-gray-300 dark:hover:text-gray-900';
+
+  return (
+    <div className='ThemeSelector w-full'>
+      <div className='my-2 flex w-full rounded border border-neutral-500 dark:border-neutral-100'>
+        <Tippy content='System theme' delay={500} placement='top'>
+          <button
+            className={classNames(themeButtonStyle, {
+              [themeButtonActiveStyle]: activeTheme === '',
+              [themeButtonInactiveStyle]: activeTheme !== '',
+            })}
+            onClick={() => {
+              onThemeChange('');
+            }}
+          >
+            <MdComputer />
+          </button>
+        </Tippy>
+        <Tippy content='Light theme' delay={500} placement='top'>
+          <button
+            className={classNames(themeButtonStyle, {
+              [themeButtonActiveStyle]: activeTheme === 'light',
+              [themeButtonInactiveStyle]: activeTheme !== 'light',
+            })}
+            onClick={() => {
+              onThemeChange('light');
+            }}
+          >
+            <LuSun />
+          </button>
+        </Tippy>
+        <Tippy content='Dark theme' delay={500} placement='top'>
+          <button
+            className={classNames(themeButtonStyle, {
+              [themeButtonActiveStyle]: activeTheme === 'dark',
+              [themeButtonInactiveStyle]: activeTheme !== 'dark',
+            })}
+            onClick={() => {
+              onThemeChange('dark');
+            }}
+          >
+            <LuMoon />
+          </button>
+        </Tippy>
+      </div>
+    </div>
+  );
+}
--- src/components/nav/ProfileMenu.js
+++ src/components/nav/ProfileMenu.js
@@ -3,12 +3,16 @@ import { Menu, MenuButton, MenuItem } from '@szhsin/react-menu';
 import { NavLink } from 'react-router-dom';
 
 import Avatar from '../common/Avatar';
+import ThemeSelector from '../common/ThemeSelector';
 
 export default function ProfileMenu() {
   const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   const menuItemClassName =
     'flex items-center space-x-2 text-text-primary hover:bg-gray-200 dark:bg-gray-700 px-4 py-1 hover:dark:bg-gray-500 cursor-pointer';
+
+  const menuItemNoHoverClassName =
+    'flex items-center space-x-2 text-text-primary dark:bg-gray-700 px-4 py-1 cursor-pointer';
 
   return (
     <div className='ProfileMenu text-black dark:text-gray-100'>
@@ -22,8 +26,13 @@ export default function ProfileMenu() {
         }
         menuClassName='bg-white dark:bg-gray-700 shadow-panel rounded-md py-2 w-36'
       >
+        <MenuItem className={menuItemNoHoverClassName} disabled>
+          <ThemeSelector />
+        </MenuItem>
         <MenuItem className={menuItemClassName}>
-          <NavLink to='/profile'>Profile</NavLink>
+          <NavLink className='size-full' to='/profile'>
+            Profile
+          </NavLink>
         </MenuItem>
         <MenuItem className={menuItemClassName} onClick={signOut}>
           Sign out
