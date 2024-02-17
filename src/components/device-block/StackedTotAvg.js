18f03fdcca359ec27c995a2a33d31c45d9058567
Matt Jones <bigboxer23@gmail.com>
2024-02-17 14:05:56 -0600

    feat: add voltage and current info to devices (#219)

--- src/components/device-block/StackedTotAvg.js
+++ src/components/device-block/StackedTotAvg.js
@@ -1,26 +1,19 @@
 import classNames from 'classnames';
-import { FormattedNumber } from 'react-intl';
+
+import StackedStatBlock from './StackedStatBlock';
 
 export default function StackedTotAvg({ total, avg, className }) {
-  const style = classNames('StackedTotAvg flex flex-col', className);
+  const style = classNames('StackedTotAvg', className);
 
   return (
-    <div className={style}>
-      <div
-        className='flex flex-row flex-wrap space-x-1 text-end text-sm text-black dark:text-gray-100
-      xs:text-base'
-      >
-        <div>Total:</div>
-        <div>
-          <FormattedNumber value={total} /> kWH
-        </div>
-      </div>
-      <div className='average-output flex flex-row flex-wrap space-x-1 text-end text-base font-bold text-black dark:text-gray-100 xs:text-lg'>
-        <div>Average:</div>
-        <div>
-          <FormattedNumber value={avg} /> kW
-        </div>
-      </div>
-    </div>
+    <StackedStatBlock
+      className={style}
+      lowerTitle='Average:'
+      lowerUnit='kW'
+      lowerValue={avg}
+      upperTitle='Total:'
+      upperUnit='kWh'
+      upperValue={total}
+    />
   );
 }
