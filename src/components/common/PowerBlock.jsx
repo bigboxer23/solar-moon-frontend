885248902f23f8ac8ad326e5ae60634ff42fc0ff
Matt Jones <bigboxer23@gmail.com>
2024-02-21 13:33:17 -0600

    feat: tweak MW precision from tenth to hundredth

--- src/components/common/PowerBlock.jsx
+++ src/components/common/PowerBlock.jsx
@@ -6,7 +6,7 @@ export default function PowerBlock({ className, power, title }) {
   const isMw = power > 1000;
   const unit = isMw ? 'MW' : 'kW';
   const powerValue = isMw ? power / 1000 : power;
-
+  const decimals = isMw ? 100 : 10;
   return (
     <div
       className={classNames(
@@ -15,7 +15,7 @@ export default function PowerBlock({ className, power, title }) {
       )}
     >
       <div className='inline-block self-end text-5xl font-bold leading-[3rem]'>
-        {Math.round(powerValue * 10) / 10}
+        {Math.round(powerValue * decimals) / decimals}
       </div>
       <div className='mb-1 flex max-w-[3.3rem] flex-col items-start justify-end text-base font-bold leading-[1.125rem]'>
         <div className='text-gray-400'>{unit}</div>
