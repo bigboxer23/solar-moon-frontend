a68d8ef0252f44f7436bb72b1b9a92ee86e29fd4
Matt Jones <bigboxer23@gmail.com>
2024-03-12 10:46:56 -0500

    fix: displaying wrong unit label for total on desktop for overview and site page (#230)

--- src/components/common/PowerBlock.jsx
+++ src/components/common/PowerBlock.jsx
@@ -2,9 +2,9 @@ import classNames from 'classnames';
 
 import PowerIcon from './PowerIcon';
 
-export default function PowerBlock({ className, power, title }) {
+export default function PowerBlock({ className, power, title, unit = 'W' }) {
   const isMw = power > 1000;
-  const unit = isMw ? 'MW' : 'kW';
+  const unitPrefix = isMw ? 'M' : 'k';
   const powerValue = isMw ? power / 1000 : power;
   const decimals = isMw ? 100 : 10;
   return (
@@ -18,7 +18,7 @@ export default function PowerBlock({ className, power, title }) {
         {Math.round(powerValue * decimals) / decimals}
       </div>
       <div className='mb-1 flex max-w-[3.3rem] flex-col items-start justify-end text-base font-bold leading-[1.125rem]'>
-        <div className='text-gray-400'>{unit}</div>
+        <div className='text-gray-400'>{unitPrefix + unit}</div>
         <div>{title}</div>
       </div>
     </div>
