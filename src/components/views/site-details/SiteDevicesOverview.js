226cd3c50c181e682509af68d0e272974ad35e17
Matt Jones <bigboxer23@gmail.com>
2024-03-29 10:33:13 -0500

    fix: alert filtering when clicking on alerts from various places in the interface wasn't consistent. Sometimes it worked, sometimes not. Filtering was not properly applied on the device or site level. Xs behaved differently than larger screens (#246)

--- src/components/views/site-details/SiteDevicesOverview.js
+++ src/components/views/site-details/SiteDevicesOverview.js
@@ -29,6 +29,8 @@ export default function SiteDevicesOverview({
   timeSeriesData,
   maxData,
 }) {
+  const navigate = useNavigate();
+
   return (
     <div className='SiteDevicesOverview w-full'>
       <div>
@@ -79,6 +81,7 @@ export default function SiteDevicesOverview({
                     }
                     className='items-end justify-center'
                     key={3}
+                    onClick={() => navigate('/alerts?deviceId=' + device.id)}
                     resolvedAlerts={
                       resolvedSiteAlerts.filter((d) => d.deviceId === device.id)
                         .length
