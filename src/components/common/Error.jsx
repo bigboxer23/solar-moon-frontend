333306e1f700413b863e62037e343dd14df8899b
Matt Jones <bigboxer23@gmail.com>
2024-04-12 09:39:42 -0500

    feat: add error page to display if there's trouble loading the overview page (#264)

---
+++ src/components/common/Error.jsx
@@ -0,0 +1,24 @@
+export default function Error({ className = '' }) {
+  const reloadPage = function () {
+    window.location.href = '/';
+  };
+
+  return (
+    <div className={`${className} Error my-8 flex justify-center`}>
+      <p className='text-center text-gray-700 dark:text-gray-300'>
+        We&apos;re sorry, there&apos;s some trouble loading your data at
+        present.
+        <br /> Please click{' '}
+        <span>
+          <button
+            className='font-bold text-gray-400 underline'
+            onClick={reloadPage}
+          >
+            here
+          </button>
+        </span>{' '}
+        or refresh the page to try again.
+      </p>
+    </div>
+  );
+}
