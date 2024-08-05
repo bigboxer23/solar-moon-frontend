6bf26ae5e62061bb351bfd18a4563dc31f52186a
Matt Jones <bigboxer23@gmail.com>
2024-08-05 15:29:54 -0500

    fix: amplify sign in/creation/reset pw fixes

--- src/components/login/SignInFooter.js
+++ src/components/login/SignInFooter.js
@@ -5,7 +5,7 @@ export function SignInFooter() {
   const { tokens } = useTheme();
 
   return (
-    <Flex justifyContent='center' padding={`0 0 ${tokens.space.medium}`}>
+    <Flex justifyContent='center' padding={`${tokens.space.xs} 0 0 0`}>
       <Link onClick={toForgotPassword}>Reset your password</Link>
     </Flex>
   );
