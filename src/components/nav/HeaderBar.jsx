40bf3c57f5ac44835212c142908cb067c68341dc
Matt Jones <bigboxer23@gmail.com>
2024-12-24 12:32:52 -0600

    feat: add logout button on pricing page so you're not trapped on an inactive account

--- src/components/nav/HeaderBar.jsx
+++ src/components/nav/HeaderBar.jsx
@@ -1,12 +1,13 @@
 import logo from '../../assets/logo.svg';
 
-export default function HeaderBar({ headerText }) {
+export default function HeaderBar({ headerText, leftContent }) {
   return (
     <div
       className='
     Navbar flex h-[4.5rem] w-full items-center border-b border-gray-400 dark:border-0 sm:h-[6.25rem]'
     >
       <div className='flex items-center justify-center'>
+        {leftContent}
         <img
           alt='brand'
           className='ml-6 size-10 sm:ml-8 sm:size-12'
