7b06c4a2f18967b2c00d507af9680a7569cf57a3
Matt Jones <bigboxer23@gmail.com>
2025-09-17 20:29:38 -0500

    chore: updating to current version

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,11 @@
 /* eslint-env jest */
 import '@testing-library/jest-dom';
 
+// Polyfills for React Router v7
+import { TextDecoder, TextEncoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 jest.mock('@aws-amplify/ui-react/styles.css', () => ({}), { virtual: true });
 jest.mock('chartjs-adapter-date-fns', () => ({}));
