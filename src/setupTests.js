a11f1d8dcb4b75b02952628febec3c8c14280fc0
Matt Jones <bigboxer23@gmail.com>
2025-12-03 20:09:30 -0600

    feat: update uuid lib

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,9 +3,28 @@ import '@testing-library/jest-dom';
 
 // Polyfills for React Router v7
 import { TextDecoder, TextEncoder } from 'util';
+import { webcrypto } from 'crypto';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
+// Polyfill for uuid v13+ (requires Web Crypto API)
+if (!global.crypto) {
+  global.crypto = webcrypto;
+}
+
 jest.mock('@aws-amplify/ui-react/styles.css', () => ({}), { virtual: true });
 jest.mock('chartjs-adapter-date-fns', () => ({}));
+jest.mock('react-data-grid/lib/styles.css', () => ({}), { virtual: true });
+jest.mock('react-day-picker/dist/style.css', () => ({}), { virtual: true });
+jest.mock('react-toastify/dist/ReactToastify.css', () => ({}), {
+  virtual: true,
+});
+jest.mock(
+  '@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css',
+  () => ({}),
+  { virtual: true },
+);
+jest.mock('react-calendar/dist/Calendar.css', () => ({}), {
+  virtual: true,
+});
